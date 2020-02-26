ad028dc07a76cb89b46be28d2569ce6cc256e914
moontracer <themoontracer@gmail.com>
2020-02-26 08:47:52 -0500

    almost done map query

--- Controllers/MapVideoTagController.cs
+++ Controllers/MapVideoTagController.cs
@@ -56,17 +56,54 @@ namespace buttoncheckDevAPI.Controllers
             return mappedTagItem.ToList();
         }
         [HttpGet("findVideo/{searchTagID?}/{searchVideoIDs?}")]
-        public ActionResult<IEnumerable<MapVideoTag>> FindVideoTags([FromQuery]int[] searchVideoIDs, [FromQuery]string searchTagID)
+        public ActionResult<IEnumerable<MapVideoTag>> FindVideoTags([FromQuery]string[] searchVideoIDs, [FromQuery]string[] searchTagID)
         {
             //this item will be used to access the mapped tags
             var mappedTagItem = from mappedtag in _context.MapVideoTag select mappedtag;
-            var parseTagID = int.Parse(searchTagID);
-            if (!String.IsNullOrEmpty(searchTagID))
+            /*
+            for (int i = 0; i < searchTagID.Length; i++)
             {
-                mappedTagItem = mappedTagItem.Where(t => t.TagId == parseTagID);
-                for (int i = 0; i < searchVideoIDs[i]; i++)
+                var parsedTag = int.Parse(searchTagID[i]);
+                if(searchTagID[i] != "")
+                {
+                    mappedTagItem = mappedTagItem.Where(v => v.TagId == parsedTag);
+                }
+                for (int x = 0; x < searchVideoIDs.Length; x++)
                 {
                     mappedTagItem = mappedTagItem.Where(v => v.VideoId == searchVideoIDs[i]);
+                }
+            }
+
+            ex. video 1 and video 2
+            5 tags
+            10 iterations in total
+            */
+            /*
+             Iteration without an index - is foreach what I'm looking for?
+
+            Update - so I'm able to get the values by themselves when searching for
+            tags but I can't get values together which is unfortunate.
+            For example,
+            http://localhost:5000/api/MapVideoTag/findVideo/?searchTagID=3&searchVideoIDs=1
+            This command works when I enter in one video ID but when I enter in both
+            ID 1 and 2 it fails to return both
+             */
+            foreach (string videoID in searchVideoIDs)
+            {
+                bool verifyVideo = !String.IsNullOrEmpty(videoID);
+                foreach (string tagID in searchTagID)
+                {
+                    bool verifyTag = !String.IsNullOrEmpty(tagID);
+                    if (verifyTag == true && verifyVideo == true)
+                    {
+                        var parsedVideo = int.Parse(videoID);
+                        var parsedTag = int.Parse(tagID);
+                        mappedTagItem = mappedTagItem.Where(v =>
+                        (v.VideoId == parsedVideo)
+                        &&
+                        (v.TagId == parsedTag)
+                        );
+                    }
                 }
             }
             return mappedTagItem.ToList();
